f4a62649317391842d076077ab9684d05a136417
jojok <valentinojaja@gmail.com>
2024-04-09 13:14:31 +0700

    fux new

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,24 +6,9 @@ import 'sweetalert2/src/sweetalert2.scss'
 import 'leaflet-arrowheads';
 import 'leaflet-polylinedecorator';
 import 'leaflet-rotatedmarker';
-import jszip from 'jszip';
-import pdfmake from 'pdfmake';
+
 import DataTable from 'datatables.net-bs5';
-import 'datatables.net-autofill-bs5';
-import 'datatables.net-buttons-bs5';
-import 'datatables.net-buttons/js/buttons.colVis.mjs';
-import 'datatables.net-buttons/js/buttons.html5.mjs';
-import 'datatables.net-buttons/js/buttons.print.mjs';
-import 'datatables.net-colreorder-bs5';
+
 import DateTime from 'datatables.net-datetime';
 import 'datatables.net-fixedcolumns-bs5';
 import 'datatables.net-fixedheader-bs5';
-import 'datatables.net-keytable-bs5';
-import 'datatables.net-responsive-bs5';
-import 'datatables.net-rowgroup-bs5';
-import 'datatables.net-rowreorder-bs5';
-import 'datatables.net-scroller-bs5';
-import 'datatables.net-searchbuilder-bs5';
-import 'datatables.net-searchpanes-bs5';
-import 'datatables.net-select-bs5';
-import 'datatables.net-staterestore-bs5';
